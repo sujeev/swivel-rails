7e1379045552ededb85d3caf296bcb6f2dcc2837
sujeev <sujeev@gmail.com>
2023-10-24 21:39:48 +0530

    minor changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,4 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format =~ %r{application/json} }
 
-  # before_action :configure_permitted_parameters, if: :devise_controller?
-  
-  protected
-  
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password])
-  #   devise_parameter_sanitizer.permit(:account_update, keys: %i[name avatar])
-  # end  
 end
